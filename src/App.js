59c53bea8b738a367908850ba5ef440fddfcb724
klaytonfantin <klaytonfantin@gmail.com>
2018-12-23 10:04:37 -0200

    Estrutura HTML e SCSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
+import react from './Site'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-        
-      
+        <Site />
       </div>
     );
   }
